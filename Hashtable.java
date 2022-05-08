2a7e9b09df4133d14d1c28d94d3048d18a9db614
DikhanbayevS <97147135+DikhanbayevS@users.noreply.github.com>
2022-05-08 20:12:14 +0300

    Update Hashtable.java

--- Hashtable.java
+++ Hashtable.java
@@ -1,11 +1,70 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.Objects;
 
-public class HashTable {
-    private final Data[] table;
+public class MyHashTable <K,V> {
+    private ArrayList<HashNode<K,V>> bucketArray; // array with chain of nodes; K - key, V - value;
+    private int numBuckets; // capacity of array
 
-    public HashTable(int tableSize) {
-        this.table = new Data[tableSize];
+    private int size; // actual number or size of elements
+
+    public MyHashTable(){
+        bucketArray = new ArrayList<>();
+        numBuckets = 10;
+        size = 0;
+
+        for (int i = 0; i < numBuckets; i++){
+            bucketArray.add(null); // filling empty array with null elements
+        }
     }
 
-    public void placeData(Data data) {
-        int index = data.hashCode() % table.length;
+    // size() - get size
+    public int size(){
+        return size;
+    }
+
+    // isEmpty() - checking empty or not -> return true or false
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+
+    public final int hashCode(K key){
+        return Objects.hashCode(key);
+    }
+	
+    private int getBucketIndex(K key){
+        int hashCode = hashCode(key);
+        int index = hashCode % numBuckets;
+
+        index = (index < 0) ? index * (-1) : index; //  multiplying by (-1) if index negative
+        return index;
+    }
+	
+	
+    public V remove (K key){
+        int bucketIndex = getBucketIndex(key);
+        int hashCode = hashCode(key);
+
+        HashNode<K,V> head = bucketArray.get(bucketIndex);
+
+        HashNode<K,V> prev = null; // extra node for connecting prev node with current node
+        while (head != null){
+            if (head.key.equals(key) && hashCode == head.hashCode){
+                break;
+            }
+
+            prev = head;
+            head = head.next;
+        }
+
+        if (head == null){
+            return null;
+        }
+
+        size--;
+
+        if (prev != null) {prev.next = head.next;} //  element - middle
+        else {bucketArray.set(bucketIndex, head.next);} //  element - beginning
+        return head.value;
+    }
+	
