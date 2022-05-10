76ebc4b00e649260d5edf579dc150e15b20f83ca
DikhanbayevS <97147135+DikhanbayevS@users.noreply.github.com>
2022-05-10 20:53:21 +0300

    Update BSTreeTest.java

--- BSTreeTest.java
+++ BSTreeTest.java
@@ -13,3 +13,9 @@ public class MyBSTreeTest {
         root = bst.insert(root, 70);
         System.out.printf("\nFirst Tree\n");
  bst.inorder(root);
+         root = bst.deleteNode(root, 50);
+        root = bst.deleteNode(root, 70);
+        System.out.printf("\nSecond Tree\n");
+        bst.inorder(root);
+    }
+}
