f954c214fb8820c0098e88b385cc49dac36640f0
DikhanbayevS <97147135+DikhanbayevS@users.noreply.github.com>
2022-05-10 20:51:53 +0300

    Create BSTreeTest.java

---
+++ BSTreeTest.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class MyBSTreeTest {
+    public static void main(String[] args){
+        Node root = null;
+        MyBSTree bst = new MyBSTree();
+        root = bst.insert(root, 80);
+        root = bst.insert(root, 30);
+        root = bst.insert(root, 20);
+        root = bst.insert(root, 50);
+        root = bst.insert(root, 40);
+        root = bst.insert(root, 60);
+        root = bst.insert(root, 70);
+        System.out.printf("\nFirst Tree\n");
